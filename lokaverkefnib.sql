e5040b0c5ba468d8ebf64b330dab01bcb6eef538
ranra <arnarar292@nemi.tskoli.is>
2017-04-27 17:28:02 +0000

    kominn af stað

--- lokaverkefnib.sql
+++ lokaverkefnib.sql
@@ -1,15 +1,23 @@
 use 2209922929_lokaverkefni;
 #a)Sýnið öll lög á tilteknum diski (þið veljið titilinn sjálf).
-
-
-
-
-
+select Cd.nafn,Lag.nafn
+from Cd
+inner join Lag
+where Lag.lag_cd=Cd.id and Cd.nafn like "Random Album Title"
+;
+#ég valdi random album title
 
 #b) Sýnið öll lög tiltekins flytjanda (þið veljið flytjandann sjálf).
+select artist.nafn,Lag.nafn
+from artist
+inner join Lag
+where Lag.lag_artist=artist.id and artist.nafn like "ac/dc"
+
+
 
 
 #c) Sýnið öll lög tiltekinnar tegundar tónlistar (þið veljið tegundina sjálf).
+
 
 
 #d) Sýnið öll lög sem taka meira en 5 mín í spilun.
