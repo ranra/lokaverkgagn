bca435f00200647d4cf8bdad2304cd3dedc1ea59
ranra <arnarar292@nemi.tskoli.is>
2017-04-27 16:47:35 +0000

    byrjaður á hluta B

---
+++ lokaverkefnib.sql
@@ -0,0 +1,51 @@
+use 2209922929_lokaverkefni;
+#a)Sýnið öll lög á tilteknum diski (þið veljið titilinn sjálf).
+
+
+
+
+
+
+#b) Sýnið öll lög tiltekins flytjanda (þið veljið flytjandann sjálf).
+
+
+#c) Sýnið öll lög tiltekinnar tegundar tónlistar (þið veljið tegundina sjálf).
+
+
+#d) Sýnið öll lög sem taka meira en 5 mín í spilun.
+
+
+#e) Sýnið alla diska sem eru gefnir út eftir árið 2010.
+
+
+#f) Sýnið nafn diska, nafn flytjanda og útgáfuár flytjanda fyrir fyrstu tvo útgáfufyrirtækin í stafrófinu.
+
+
+#g) Sýnið fimm lengstu lögin, nafn flytjanda og nafn útgefanda.
+
+
+#h) Sýnið fjölda diska á tegund – birtið bara þær tvær tegundir sem hafa flesta diska.
+
+
+#i) Sýnið þann útgefanda sem hefur gefið út fæsta diska.
+
+
+#j) Sýnið nafn, tegund, útgefanda allra diska sem gefnir eru út á árunum 1998-2017. Hópað saman
+
+
+#eftir útgefanda.
+
+
+#k) Sýnið hversu mörg lög byrja á bókstafnum A og hversu mörg lög hafa bókstafinn s í nafninu.
+
+
+#l) Sýndu nafn og aldur allra flytjanda.
+
+
+#m) Sýndu meðallengd allra laganna.
+
+
+#n) Sýndu hversu margir flytjendur eiga fleiri en 4 lög.
+
+
+#o) Sýndu elsta flytjandann og nöfn allra hans laga.
