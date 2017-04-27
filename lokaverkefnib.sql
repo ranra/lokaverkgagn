2368dc79f6b49d66dacc9aa0a557b185044ca269
ranra <arnarar292@nemi.tskoli.is>
2017-04-27 18:29:36 +0000

    fleiri dæmi leyst

--- lokaverkefnib.sql
+++ lokaverkefnib.sql
@@ -1,35 +1,53 @@
 use 2209922929_lokaverkefni;
 #a)Sýnið öll lög á tilteknum diski (þið veljið titilinn sjálf).
-select Cd.nafn,Lag.nafn
+select Cd.nafn As Cd,Log.nafn as Lag
 from Cd
-inner join Lag
-where Lag.lag_cd=Cd.id and Cd.nafn like "Random Album Title"
+inner join Log
+where Log.log_cd=Cd.id and Cd.nafn like "Random Album Title"
 ;
-#ég valdi random album title
 
 #b) Sýnið öll lög tiltekins flytjanda (þið veljið flytjandann sjálf).
-select artist.nafn,Lag.nafn
+select artist.nafn as Flytjandi,Log.nafn as Lag
 from artist
-inner join Lag
-where Lag.lag_artist=artist.id and artist.nafn like "ac/dc"
-
-
+inner join Log
+where Log.log_artist=artist.id and artist.nafn like "ac/dc"
+;
 
 
 #c) Sýnið öll lög tiltekinnar tegundar tónlistar (þið veljið tegundina sjálf).
-
+select artist.nafn as Band,log.nafn as Lag,cd.nafn as CD,tegund.tegund
+from log
+inner join tegund,artist,cd
+where log.log_artist=artist.id and log.log_tegund =tegund.id and log.log_cd =cd.id and tegund.tegund like "Pop"
+;
 
 
 #d) Sýnið öll lög sem taka meira en 5 mín í spilun.
-
+select log.nafn as Lag,artist.nafn as Flytjandi,log.lengd
+from log
+inner join artist
+where  log.log_artist=artist.id and lengd > 0500
+;
 
 #e) Sýnið alla diska sem eru gefnir út eftir árið 2010.
-
+select nafn as Cd,utgafudagur
+from Cd
+where utgafudagur >"2010"
+;
 
 #f) Sýnið nafn diska, nafn flytjanda og útgáfuár flytjanda fyrir fyrstu tvo útgáfufyrirtækin í stafrófinu.
+#("Albert/Atlantic"),
+#("Brother/Capitol"),
+
+select artist.nafn,cd.nafn,cd.utgafudagur,utgefandi.nafn
+from cd
+inner join artist,utgefandi
+where cd.cd_artist =artist.id and cd.cd_utgefandi =utgefandi.id and utgefandi.nafn <"c"
+;
 
 
 #g) Sýnið fimm lengstu lögin, nafn flytjanda og nafn útgefanda.
+
 
 
 #h) Sýnið fjölda diska á tegund – birtið bara þær tvær tegundir sem hafa flesta diska.
