471f5f9fa5fa8e5cd1c77226c07485e84b47b610
ranra <arnarar292@nemi.tskoli.is>
2017-04-29 16:04:33 +0000

    gagnagrunnur =fullkominn

--- Lokaverkefni.sql
+++ Lokaverkefni.sql
@@ -51,8 +51,8 @@ faedingardagur date null,
 lifandi varchar(255) null,
 latinn date null,
 lysing varchar(255)null,
-tilheyrir_artist int not null,
-foreign key (tilheyrir_artist)
+medlimir_artist int not null,
+foreign key (medlimir_artist)
 	references Artist(id)
 
 );
@@ -124,33 +124,33 @@ values
 insert into
 Artist(nafn,stofndagur,starfandi,lok,artist_flokkur)
 values
-("joeyBada$$",12/6/1999,"ja",null,1),
-("Deadmau5",6/26/2005,"ja",null,3),
-("AC/DC",null/null/1973,"ja",null,2),
-("Beach boys",null/null/1961,"ja",null,3),
-("Bee gees",null/null/1958,"nei",null/null/2012,3)
+("joeyBada$$","1999-12-6","ja",null,1),
+("Deadmau5","2005-5-26","ja",null,3),
+("AC/DC","1973-1-1","ja",null,2),
+("Beach boys","1961-1-1","ja",null,3),
+("Bee gees","1958-1-1","nei",null/null/2012,3)
 ;
 
 
 insert into
-Medlimir(nafn,faedingardagur,lifandi,latinn,lysing,tilheyrir_artist)
+Medlimir(nafn,faedingardagur,lifandi,latinn,lysing,medlimir_artist)
 values
-("Jo-Vaughn Virginie Scott",20/1/1995,"ja",null,"Jo-Vaughn Virginie Scott (born January 20, 1995),[2] better known by his stage name Joey Bada$$, is an American rapper and actor",1),
-("Joel Thomas Zimmerman",1/5/1981,"ja",null,"JoelThomasZimmerman (born January 5, 1981), better known by his stage name deadmau5 (pronounced dead mouse), is a Canadian record producer and DJ from Toronto, Ontario.",2),
-("Bon Scott",7/26/1946,"nei",2/19/1980,"Ronald Belford Scott (9 July 1946 – 19 February 1980) was an Australian rock musician, best known for being the lead singer and lyricist of Australian hard rock band AC/DC from 1974 until his death in 1980",3),
-("Angus Young",3/31/1955,"ja",null,"Angus McKinnon Young (born 31 March 1955) is a Scottish guitarist, best known as the co-founder, lead guitarist, songwriter and only remaining original member of the Australian hard rock band AC/DC",3),
-("Malcom Young",1/6/1953,"ja",null,"Malcolm Mitchell Young (born 6 January 1953)[1] is a Scottish retired musician and songwriter, best known as a co-founder, rhythm guitarist, backing vocalist and songwriter for the hard rock band AC/DC.",3),
-("Cliff Williams",12/14/1949,"ja",null,"Clifford Williams (born 14 December 1949) is a British musician who was a member of the Australian hard rock band AC/DC as their bassist and backing vocalist from 1977 to 2016",3),
-("Phil Rudd",5/19/1954,"ja",null,"Phillip Hugh Norman Rudd (born Phillip Hugh Norman Witschke Rudzevecuis,[1][2] 19 May 1954) is an Australian drummer, best known for his membership in Australian hard rock band AC/DC from 1975 through 1983",3),
-("Dennis Willson",12/4/1944,"nei",12/28/1983,"Dennis Carl Wilson (December 4, 1944 – December 28, 1983) was an American musician, singer, and songwriter who co-founded the Beach Boys",4),
-("Carl Willson",12/21/1946,"nei",2/6/1998,"Carl Dean Wilson (December 21, 1946 – February 6, 1998) was an American musician, singer, songwriter, and record producer who co-founded the Beach Boys",4),
-("Ricky Fataar",9/5/1952,"ja",null,"Ricky Fataar (born 5 September 1952) is a South African multi-instrumentalist of Cape Malay descent, who has performed as both a drummer and a guitarist",4),
-("Blondie Chaplin",7/7/1951,"ja",null,"Terence William Chaplin (born 7 July 1951) is a singer and guitarist from Durban, South Africa, where he played in the band The Flames in the mid-to late-1960s",4),
-("Barry Gibb",9/1/1946,"ja",null,"Barry Alan Crompton Gibb, CBE (born 1 September 1946) is a British singer, songwriter and record producer who rose to worldwide fame as a co-founder of the group Bee Gees",5),
-("Robin Gibb",12/22/1949,"nei",5/20/2012,"Robin Hugh Gibb CBE (22 December 1949 – 20 May 2012) was a British singer, songwriter and record producer, who gained worldwide fame as a member of the pop group the Bee Gees",5),
-("Maurice Gibb",12/22/1949,"nei",1/12/2003,"Maurice Ernest Gibb, CBE (/ˈmɔːrɪs/; 22 December 1949 – 12 January 2003) was a British singer, songwriter, multi-instrumentalist, and record producer, who achieved fame as a member of the British pop group the Bee Gees",5),
-("Alan Kendall",9/6/1944,"ja",null,"Alan Kendall (born 9 September 1944, in Darwen, Lancashire) is an English musician and was the lead guitarist for the popular band the Bee Gees",5),
-("Blue Weaver",3/11/1947,"ja",null,"played behind the Bee Gees at the initiation of his former Amen Corner colleague drummer Dennis Bryon, who had joined their backing band a year earlier during their successful 1975-79 period",5),
+("Jo-Vaughn Virginie Scott","1995-1-20","ja",null,"Jo-Vaughn Virginie Scott (born January 20, 1995),[2] better known by his stage name Joey Bada$$, is an American rapper and actor",1),
+("Joel Thomas Zimmerman","1981-1-5","ja",null,"JoelThomasZimmerman (born January 5, 1981), better known by his stage name deadmau5 (pronounced dead mouse), is a Canadian record producer and DJ from Toronto, Ontario.",2),
+("Bon Scott","1946-7-26","nei","1980-2-19","Ronald Belford Scott (9 July 1946 – 19 February 1980) was an Australian rock musician, best known for being the lead singer and lyricist of Australian hard rock band AC/DC from 1974 until his death in 1980",3),
+("Angus Young","1955-3-31","ja",null,"Angus McKinnon Young (born 31 March 1955) is a Scottish guitarist, best known as the co-founder, lead guitarist, songwriter and only remaining original member of the Australian hard rock band AC/DC",3),
+("Malcom Young","1953-1-6","ja",null,"Malcolm Mitchell Young (born 6 January 1953)[1] is a Scottish retired musician and songwriter, best known as a co-founder, rhythm guitarist, backing vocalist and songwriter for the hard rock band AC/DC.",3),
+("Cliff Williams","1949-12-14","ja",null,"Clifford Williams (born 14 December 1949) is a British musician who was a member of the Australian hard rock band AC/DC as their bassist and backing vocalist from 1977 to 2016",3),
+("Phil Rudd","1954-5-19","ja",null,"Phillip Hugh Norman Rudd (born Phillip Hugh Norman Witschke Rudzevecuis,[1][2] 19 May 1954) is an Australian drummer, best known for his membership in Australian hard rock band AC/DC from 1975 through 1983",3),
+("Dennis Willson","1944-12-4","nei","1983-12-28","Dennis Carl Wilson (December 4, 1944 – December 28, 1983) was an American musician, singer, and songwriter who co-founded the Beach Boys",4),
+("Carl Willson","1946-12-21","nei","1998-2-6","Carl Dean Wilson (December 21, 1946 – February 6, 1998) was an American musician, singer, songwriter, and record producer who co-founded the Beach Boys",4),
+("Ricky Fataar","1952-9-5","ja",null,"Ricky Fataar (born 5 September 1952) is a South African multi-instrumentalist of Cape Malay descent, who has performed as both a drummer and a guitarist",4),
+("Blondie Chaplin","1951-7-7","ja",null,"Terence William Chaplin (born 7 July 1951) is a singer and guitarist from Durban, South Africa, where he played in the band The Flames in the mid-to late-1960s",4),
+("Barry Gibb","1946-9-1","ja",null,"Barry Alan Crompton Gibb, CBE (born 1 September 1946) is a British singer, songwriter and record producer who rose to worldwide fame as a co-founder of the group Bee Gees",5),
+("Robin Gibb","1949-12-22","nei","2012-5-20","Robin Hugh Gibb CBE (22 December 1949 – 20 May 2012) was a British singer, songwriter and record producer, who gained worldwide fame as a member of the pop group the Bee Gees",5),
+("Maurice Gibb","1949-12-22","nei","2003-1-12","Maurice Ernest Gibb, CBE (/ˈmɔːrɪs/; 22 December 1949 – 12 January 2003) was a British singer, songwriter, multi-instrumentalist, and record producer, who achieved fame as a member of the British pop group the Bee Gees",5),
+("Alan Kendall","1944-9-6","ja",null,"Alan Kendall (born 9 September 1944, in Darwen, Lancashire) is an English musician and was the lead guitarist for the popular band the Bee Gees",5),
+("Blue Weaver","1947-3-11","ja",null,"played behind the Bee Gees at the initiation of his former Amen Corner colleague drummer Dennis Bryon, who had joined their backing band a year earlier during their successful 1975-79 period",5),
 ("Dennis Bryon",null,null,null,"Dennis Bryon is a Welsh rock drummer from Cardiff, best known for his work with the Bee Gees from 1974 to 1979.",5)
 ;
 
@@ -158,13 +158,13 @@ values
 insert into
 Cd(nafn,utgafudagur,cd_artist,cd_tegund,cd_utgefandi)
 values
-("B4.DA.$$",20/1/2015,1,4,1),
-("Random Album Title",9/8/2008,2,5,2),
-("Highway to hell",7/27/1979,3,2,3),
-("Smiley smile ",12/18/2001,4,1,4),
-("Saturday Night Fever: The Original Movie Sound Track ",11/15/1977,5,3,5),
-("All American Bada$$",4/7/2017,1,4,1),
-("Back in Black",6/25/1980,3,2,3)
+("B4.DA.$$","2015-1-20",1,4,1),
+("Random Album Title","2008-9-8",2,5,2),
+("Highway to hell","1979-7-27",3,2,3),
+("Smiley smile ","2001-12-18",4,1,4),
+("Saturday Night Fever: The Original Movie Sound Track ","1977-11-15",5,3,5),
+("All American Bada$$","2017-4-7",1,4,1),
+("Back in Black","1980-6-25",3,2,3)
 ;
 
 
